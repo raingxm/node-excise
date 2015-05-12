d0a477e4f37c5029f60785441651fd70b00617a7
zhangxu <xuzhang@thoughtworks.com>
2015-05-12 16:44:53 +0800

    use exppress, show hello world

---
+++ lesson1/app.js
@@ -0,0 +1,11 @@
+var express = require('express');
+
+var app = express();
+
+app.get('/', function(request, response) {
+    response.send('Hello World');
+});
+
+app.listen(3000, function() {
+    console.log('app is listen on port 3000');
+});
