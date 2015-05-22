2ff3be239c78966f3380388d94f8ee3dd661eeb2
raingxm <raingxm@163.com>
2015-05-22 08:46:16 +0800

    use supertest to test code, have some bugs

--- lesson8/app.js
+++ lesson8/app.js
@@ -1,8 +1,8 @@
 var express = require('express');
 
 var fibonacci = function(n) {
-    if(typeof n !== 'number' || isNaN(n)) {
-        throw new Error('n should be a number');
+    if (typeof n !== 'number' || isNaN(n)) {
+        throw new Error('n should be a Number');
     }
 
     if(n < 0) {
@@ -10,7 +10,7 @@ var fibonacci = function(n) {
     }
 
     if(n > 10) {
-        throw new Error('n should <= 10')
+        throw new Error('n should <= 10');
     }
 
     if(n === 0) {
@@ -22,18 +22,20 @@ var fibonacci = function(n) {
     }
 
     return fibonacci(n-1) + fibonacci(n-2);
-}
+};
 
 var app = express();
 
 app.get('/fib', function(req, res) {
-    var n = Number(req.query.q);
+    var n = Number(req.query.n);
     try{
         res.send(String(fibonacci(n)));
     } catch(e) {
         res.status(500).send(e.message);
     }
-})
+});
+
+module.exports = app;
 
 app.listen(3000, function() {
    console.log("server listen on 3000...");
