40165ca47a99c297a4eaab72b3cc07b6cda81d41
raingxm <raingxm@163.com>
2015-05-28 21:46:32 +0800

    add describtion to book info

--- fetchBook/app.js
+++ fetchBook/app.js
@@ -3,30 +3,69 @@ var eventproxy = require('eventproxy');
 var superagent = require('superagent-charset');
 var cheerio = require('cheerio');
 
-var amazonUrl = "book.dangdang.com";
+var dangdangUrl = "book.dangdang.com";
 var ep = new eventproxy();
 var app = new express();
 
 app.get('/', function(request, response) {
-    superagent.get(amazonUrl).charset('gbk').end(function(err, res) {
+    superagent.get(dangdangUrl).charset('gbk').end(function(err, sres) {
         if(err) {
             next(err);
         }
 
+        var detailUrls = [];
         var items = [];
-        var $ = cheerio.load(res.text);
+        var $ = cheerio.load(sres.text);
+        $('.product_ul li .name a').each(function(index, element) {
+            var $element = $(element);
+            var detailUrl = $element.attr('href');
+            detailUrls.push(detailUrl);
+        });
+
         $('.product_ul li').each(function(index, element) {
             var $element = $(element);
             var name = $element.find('.name a').text();
             var author = $element.find('.author').text();
             var price = parseInt($element.find('.price .rob .num').text());
             items.push({name: name,
-                        author: author,
-                        price: price
-                });
+                author: author,
+                price: price
+            });
         });
 
-        response.send(items);
+        detailUrls.forEach(function(detailUrl) {
+            superagent.get(detailUrl).charset('gbk').end(function(err, sres) {
+                if(err) {
+                    console.error(err);
+                }
+                ep.emit('fetch_description', [detailUrl, sres]);
+            });
+        });
+
+        ep.after('fetch_description', detailUrls.length, function(details) {
+            var describes = [];
+            var booksInfo = [];
+            describes = details.map(function(eachDetail) {
+                var detailUrl = eachDetail[0];
+                var content = eachDetail[1];
+                var $ = cheerio.load(content.text);
+                return {
+                    url: detailUrl,
+                    describe: $(".show_info_autoheight .head_title_name").text()
+                };
+            });
+            describes.forEach(function(ele, index) {
+                booksInfo.push({
+                    name: items[index].name,
+                    author: items[index].author,
+                    price: items[index].price,
+                    describe: ele.describe,
+                    url: ele.url
+                });
+            });
+            response.send(booksInfo);
+        });
+
     });
 });
 
